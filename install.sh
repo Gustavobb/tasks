67797470ceab5f8118003a28daebc5c0a122e68b
Gustavo Beltrão Braga <38335492+Gustavobb@users.noreply.github.com>
2020-11-13 23:54:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt install python3-dev libpq-dev python3-pip -y
-python3 -m pip install -r requirements.txt
-python3 manage.py migrate
+python3 -m pip install -r /home/ubuntu/tasks/requirements.txt
+python3 /home/ubuntu/tasks/manage.py migrate
 echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
 export DJANGO_SUPERUSER_PASSWORD=cloud
 export DJANGO_SUPERUSER_USERNAME=cloud
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
-python3 manage.py createsuperuser --noinput
+python3 /home/ubuntu/tasks/manage.py createsuperuser --noinput
