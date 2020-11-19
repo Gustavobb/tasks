28c378688061f6c63a13254de65dc9db8a278519
Gustavobb <Gustavobb@al.insper.edu.br>
2020-11-19 19:13:53 +0000

    updates

--- tasks/views.py
+++ tasks/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
 from tasks.models import Task
 
 # Create your views here.
-@api_view(["GET"])
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 
-@api_view(["GET"])
 def get_task(request):
     return HttpResponse(Task.objects.all())
 
-@api_view(["POST"])
 def create_task(request):
     print(request)
     return HttpResponse(request)
 
-@api_view(["POST"])
 def delete_task(request, task_id):
     pass
 
-@api_view(["POST"])
 def update_task(request, task_id):
     pass
