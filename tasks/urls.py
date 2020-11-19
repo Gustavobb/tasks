29e3d6cdc58ddf77072bccec33cbf255837fbf1d
Gustavobb <Gustavobb@al.insper.edu.br>
2020-11-19 18:40:21 +0000

    endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get/', views.get_tasks, name='get tasks'),
+    path('create/', views.create_task, name='create task'),
+    path('delete/<int:task_id>', views.delete_task, name='delete task'),
+    path('update/<int:task_id>', views.update_task, name='update task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from tasks.models import Task
 
 # Create your views here.
-
-
+@api_view(["GET"])
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+@api_view(["GET"])
+def get_task(request):
+    return HttpResponse(Task.objects.all())
+
+@api_view(["POST"])
+def create_task(request):
+    pass
+
+@api_view(["POST"])
+def delete_task(request, task_id):
+    pass
+
+@api_view(["POST"])
+def update_task(request, task_id):
+    pass
