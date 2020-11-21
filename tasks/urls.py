2f9e8c096f4cf6894751ced8099d776eb1e56026
Gustavobb <Gustavobb@al.insper.edu.br>
2020-11-21 20:01:45 +0000

    fix

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get/', views.get_tasks, name='get tasks'),
-    path('create/', views.create_task, name='create task'),
-    path('delete/<int:task_id>', views.delete_task, name='delete task'),
-    path('update/<int:task_id>', views.update_task, name='update task')
+    path('getTask/<int:task_title>', views.get_task, name='get task'),
+    path('getTasks/', views.get_tasks, name='get tasks'),
+    path('createTask/', views.create_task, name='create task'),
+    path('deleteTask/<int:task_title>', views.delete_task, name='delete task'),
+    path('deleteTasks/', views.delete_tasks, name='delete tasks')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -11,8 +11,17 @@ def index(request):
 
 def get_tasks(request):
     if request.method == 'GET':
-        serializer = TaskSerializer(Task.objects.all(), many =True)
-        return JsonResponse(serializer.data, safe =False)
+        serializer = TaskSerializer(Task.objects.all(), many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+def get_task(request, task_title):
+    if request.method == 'GET':
+        try: 
+            task = Task.objects.get(pk=task_title)
+        except Task.DoesNotExist: 
+            raise HttpResponse("Task not found")
+
+        return JsonResponse(task, safe=False, status=201)
 
 @csrf_exempt
 def create_task(request):
@@ -25,10 +34,14 @@ def create_task(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
-@csrf_exempt
-def delete_task(request, task_id):
-    pass
+def delete_task(request, task_title):
+    if request.method == 'DELETE':
+        try: Task.objects.get(pk=task_title).delete()
+        except Task.DoesNotExist: raise HttpResponse("Task not found")
 
-@csrf_exempt
-def update_task(request, task_id):
-    pass
+        return HttpResponse("Task deleted")
+
+def delete_tasks(request):
+    if request.method == 'DELETE':
+        task = Task.objects.all().delete()
+        return HttpResponse("Tasks deleted")
