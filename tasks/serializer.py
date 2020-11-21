9395e7bad59bd53f186d59513ca4e4cfd0116468
Gustavobb <Gustavobb@al.insper.edu.br>
2020-11-21 18:44:28 +0000

    fix

---
+++ tasks/serializer.py
@@ -0,0 +1,9 @@
+# serializers.py
+from rest_framework.serializers import ModelSerializer
+
+from .models import Task
+
+class TaskSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('title', 'pub_date', 'description')
--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from tasks.models import Task
+from tasks.serializer import TaskSerializer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 
 def get_tasks(request):
-    return HttpResponse(Task.objects.all())
+    if request.method == 'GET':
+        return HttpResponse(Task.objects.all())
 
 def create_task(request):
-    print(request)
-    return HttpResponse(request)
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer =TaskSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status =201)
+        return JsonResponse(serializer.errors, status = 400)
+
 
 def delete_task(request, task_id):
     pass
